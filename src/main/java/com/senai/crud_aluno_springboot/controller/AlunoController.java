11a32b497ccd729774d0d0921b70042acbe36ba9
Izabella Hermano <izabellahermano1@gmail.com>
2025-08-26 11:07:25 -0300

    Finalização do primeiro projeto springboot

--- src/main/java/com/senai/crud_aluno_springboot/controller/AlunoController.java
+++ src/main/java/com/senai/crud_aluno_springboot/controller/AlunoController.java
@@ -4,16 +4,21 @@ import com.senai.crud_aluno_springboot.model.Aluno;
 import com.senai.crud_aluno_springboot.model.AlunoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+/*
+@PostMapping: cria um aluno.
+@GetMapping: lista todos os alunos.
+@GetMapping("/{id}"): busca um aluno pelo ID.
+@PutMapping("/{id}"): atualiza um aluno.
+@DeleteMapping("/{id}"): deleta um aluno.
+*/
 
 import java.util.List;
 
 @RestController// Quando receber uma requisição REST, direcina para o controller
 @RequestMapping("/aluno")//toda requisição que recebe, faz um mapeamento dela e direciona para o lugar certo
-//mapear: pegar valor e converter para outro valor
+public class AlunoController {//mapear: pegar valor e converter para outro valor
 
-public class AlunoController {
-
-    @Autowired//Instancia o repositorio
+    @Autowired//Instancia classe automaticamente
     private AlunoRepository alunoRepository;
 
     // Criar um novo aluno
@@ -29,31 +34,29 @@ public class AlunoController {
     }
 
     //Buscar por ID
-    /*@GetMapping ("/{id}")
+    @GetMapping ("/{id}")
     public Aluno getAlunosById(@PathVariable String id) {
         return alunoRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Aluno não encontrado"));
-    }*/
+    }
 
     //Atualizar Aluno
-    /*@PutMapping("/{id}")
-    public Aluno updateAluno(@PathVariable String id, @RequestBody Aluno alunoDetails) {
-        Aluno aluno = alunoRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Aluno não encontrado"));
+    @PutMapping("/{id}")
+    public Aluno updateAluno(@PathVariable String id, @RequestBody Aluno alunoDetails){
+            Aluno aluno = alunoRepository.findById(id)
+                    .orElseThrow(() -> new RuntimeException("Aluno não encontrado"));
 
-        aluno.setNome(alunoDetails.getNome());
-        aluno.setCpf(alunoDetails.getCpf());
-        aluno.setEmail(alunoDetails.getEmail());
-        aluno.setCurso(alunoDetails.getCurso());
+            aluno.setNome(alunoDetails.getNome());
+            aluno.setCpf(alunoDetails.getCpf());
+            aluno.setEmail(alunoDetails.getEmail());
+            aluno.setCurso(alunoDetails.getCurso());
 
-        return alunoRepository.save(aluno);
-    }*/
+            return alunoRepository.save(aluno);
+        }
 
     //Excluir Aluno
     @DeleteMapping ("/{id}")
     public void deleteAluno(@PathVariable String id) {
         alunoRepository.deleteById(id);
     }
-
-
 }
--- src/main/java/com/senai/crud_aluno_springboot/model/Aluno.java
+++ src/main/java/com/senai/crud_aluno_springboot/model/Aluno.java
@@ -1,80 +1,32 @@
 package com.senai.crud_aluno_springboot.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
 
+/*@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor*/
+@Data//envolve todas as outras anotações
 @Entity
-
 public class Aluno {
+
+    //Constrains
     @Id //Anotação
     @GeneratedValue(strategy = GenerationType.UUID)// Informa a jdk que é necessario seguir essa anoção para execução do código
     private String id;
 
+    @NotBlank(message = "O nome do aluno não pode estar vazio")
+    @Column(nullable = false, length = 100)
     private String nome;
+
+    @NotBlank(message = "O CPF do aluno não pode estar vazio")/*NotBlank-> Para String | NotNull -> Para int */
+    @Column(nullable = false, length = 100)
     private String cpf;
+
     private int idAcesso;
     private String email;
     private String curso;
-
-    //Construtor
-    public Aluno(String id, String nome, String cpf, int idAcesso, String email, String curso) {
-        this.id = id;
-        this.nome = nome;
-        this.cpf = cpf;
-        this.idAcesso = idAcesso;
-        this.email = email;
-        this.curso = curso;
-    }
-
-    // Getters e Setters
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public int getIdAcesso() {
-        return idAcesso;
-    }
-
-    public void setIdAcesso(int idAcesso) {
-        this.idAcesso = idAcesso;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getCurso() {
-        return curso;
-    }
-
-    public void setCurso(String curso) {
-        this.curso = curso;
-    }
 
 }
